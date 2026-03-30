4cdf707e9288053060291217d21991f9e0eb4720
Ultrio <113686237+UltrioG@users.noreply.github.com>
2026-03-31 01:10:56 +0800

    Made window resizeable

--- conf.lua
+++ conf.lua
@@ -1,12 +1,14 @@
 function lovr.conf(T)
-	T.modules.audio		 = true
-	T.modules.data		 = true
-	T.modules.event		 = true
-	T.modules.graphics	 = true
-	T.modules.headset	 = true
-	T.modules.math		 = true
-	T.modules.physics	 = true
-	T.modules.system	 = true
-	T.modules.thread	 = true
-	T.modules.timer		 = true
+	T.modules.audio		= true
+	T.modules.data		= true
+	T.modules.event		= true
+	T.modules.graphics	= true
+	T.modules.headset	= true
+	T.modules.math		= true
+	T.modules.physics	= true
+	T.modules.system	= true
+	T.modules.thread	= true
+	T.modules.timer		= true
+	
+	T.window.resizable 	= true
 end
