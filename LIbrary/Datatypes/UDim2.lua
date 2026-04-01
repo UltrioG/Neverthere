f2f4c039e68d571cd534d5a3831e175088ed1dc8
Ultrio <113686237+UltrioG@users.noreply.github.com>
2026-04-01 12:50:37 +0800

    Moved UDim2 to Typedef

--- LIbrary/Datatypes/UDim2.lua
+++
@@ -1,8 +0,0 @@
----@meta
-
----@class UDim2
----@field xScale number?
----@field yScale number?
----@field xOffset integer?
----@field yOffset integer?
-UDim2 = {}
--- LIbrary/Typedef.lua
+++ LIbrary/Typedef.lua
@@ -2,3 +2,10 @@
 
 ---@alias colortext table
 ---@alias color number 		Preferably Hex Color
+
+---@class UDim2
+---@field xScale number?
+---@field yScale number?
+---@field xOffset integer?
+---@field yOffset integer?
+UDim2 = {}
