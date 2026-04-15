6b6cf0b00dab14702adfed9e61e79303d8d94ae3
Ultrio <113686237+UltrioG@users.noreply.github.com>
2026-04-15 16:01:51 +0800

    Added region definitions

--- LIbrary/Everything.lua
+++ LIbrary/Everything.lua
@@ -2,8 +2,10 @@ local Everything = {}
 
 Everything.		Poject		= require "Library.Poject"
 
+--#region 2D Gui
 Everything.		GUI2D		= require "Library.GUI2D"
 Everything.		Rectangle	= require "Library.Rectangle"
 Everything.		TextLabel 	= require "Library.Rectangle"
+--#endregion 2D Gui
 
 return Everything
