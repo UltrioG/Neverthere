848b80264d9f09cead51c5d8f9a73b267d98f9bf
Ultrio <113686237+UltrioG@users.noreply.github.com>
2026-04-28 02:56:34 +0800

    More renames

--- LIbrary/Prototypes/Gui/Decal.lua
+++ LIbrary/Prototypes/Gui/Decal.lua
@@ -3,19 +3,19 @@ local Rectangle = require("Library.Rectangle")
 ---@class Decal: Rectangle
 ---@field FileName string
 ---@field Prototype Decal | Rectangle
----@field ImageAlpha percentage
+---@field DecalAlpha percentage
 local Decal = Rectangle:Inherit()
 Decal.Type = "Decal"
 Decal.isPrototype = true
 
----Render this Image.
+---Render this Decal.
 ---@param pass Pass
 function Decal:Render(pass)
 	self:GetPrototype():GetPrototype().Render(self, pass)
 	local texture = lovr.graphics.newTexture(self.FileName, {})
 	local sizeX, sizeY = self:GetAbsoluteSize()
 	local x, y = self:GetAbsolutePosition()
-	pass:setColor(0xFFFFFF, self.ImageAlpha)
+	pass:setColor(0xFFFFFF, self.DecalAlpha)
 	local m4 = lovr.math.mat4(vec3(x, y, 0), vec3(-sizeX, sizeY, 0), quat(self.Rotation, 0,0,-1))
 	pass:draw(texture, m4)
 end
