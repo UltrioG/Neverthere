542cc2a8e65da90357b4590df6e867a79fbc100f
Ultrio <113686237+UltrioG@users.noreply.github.com>
2026-06-05 02:58:40 +0800

    Made pretty printed indents better

--- LIbrary/Pretty.lua
+++ LIbrary/Pretty.lua
@@ -15,19 +15,19 @@ local PRETTY = {}
 ---@return string
 function PRETTY.parse(x)
 	local s = tostring(x)
-	if type(x) == "string" then return '"'..s..'"' end
+	if type(x) == "string" then return '"'..s:gsub("\n", "\\n")..'"' end
 	if type(x) == "nil" then return "nil" end
 	if type(x) ~= "table" then return s end
 	if not s:match("table: 0x%x+") then return s end
 	
 	local s = ("(@%s) {"):format(tostring(x):match("0x%x+"))
 	for k, v in pairs(x) do
-		s = s .. '\n\t' .. ("[%s] = %s"):format(
+		s = s .. '\n' .. ("[%s] = %s"):format(
 			(type(k) == "string" and '"%s"' or "%s"):format(tostring(k)),
 			PRETTY.parse(v)
 		) .. ','
 	end
-	s = s:sub(1,-2)
+	s = s:sub(1,-2):gsub("\n", "\n\t")
 	s = s .. "\n}"
 	return s
 end
