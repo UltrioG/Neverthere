afa3efc2206a59a53e30d3034119287f6ee61fac
Ultrio <113686237+UltrioG@users.noreply.github.com>
2026-06-20 00:35:26 +0800

    Completely remade JSON Parsing

--- LIbrary/json.lua
+++ LIbrary/json.lua
@@ -1,172 +1,215 @@
 local JSON = {}
 
----@class JSON_NULL
-JSON.JSON_NULL = {}
+---@enum (key) jsonEnum
+local JSON_ENUM = {
+	arrInit = {"arrInit"},
+	arrExit = {"arrExit"},
+	objInit = {"objInit"},
+	objExit = {"objExit"},
+	comma = {"comma"},
+	colon = {"colon"},
+	null = {"null"}
+}
+
+local CONSTANTS = {
+	["null"] = JSON_ENUM.null,
+	["true"] = true,
+	["false"] = false
+}
 
 ---Parses a JSON string.
 ---@param s string
 ---@return any value The parsed value
----@return number length The length of the value string, incl. whitespace
----@return jsonType jsonType The type of the value returned
 function JSON.parse(s)
-	-- Simple values
-	if s:match("^%s*true") then return true, 4 + #s:match("^%s*"), "true" end
-	if s:match("^%s*false") then return false, 5 + #s:match("^%s*"), "false" end
-	if s:match("^%s*null") then return JSON.JSON_NULL, 4 + #s:match("^%s*"), "null" end
-
-	-- Numbers
-	if s:match("^%s*%-?%d") then
-		---@type string, string, string, string
-		local WHITESPACE, SIGN, INT, FRAC, EXP = s:match("^(%s*)(%-?)(%d+)(%.?%d*)([eE]?[+-]?%d*)")
-		SIGN = SIGN or ""
-		INT = INT or ""
-		FRAC = FRAC or ""
-		EXP = EXP or ""
-		if #INT > 1 and INT:first() == '0' then
-			error("JSON Error: Numbers cannot start with 0 but be nonzero")
-		end
-		if FRAC:lower() == "." then FRAC = "" end -- Prevents numbers such as 0.
-		if EXP:lower() == "e" then EXP = "" end -- Prevents numbers such as 123E
-		local n = tonumber(SIGN .. INT .. FRAC .. EXP)
-		return n, #SIGN + #INT + #FRAC + #EXP + #WHITESPACE, "number"
+	---@type [jsonEnum | string | true | false | number]
+	local intermediate = {}
+	---@type [ [integer, integer] ]
+	local intermediatePositions = {}
+	local index = 1
+	local line = 1
+	local lastRecordedChar = 1
+	local function push(x) table.insert(intermediate, x) end
+	local function pushIndex()
+		table.insert(intermediatePositions, {line, index - lastRecordedChar})
 	end
-
-	-- Strings
-	if s:match('^%s*"') then
-		local foundString = ""
-		local index = s:find('"') + 1
-		local isControlCharMode = false
-		while true do
-			local currentChar = s:sub(index, index)
-			if isControlCharMode then goto controlChar end
-			if currentChar == '\\' then
-				isControlCharMode = true
-				goto continue
+	local function currentChar() return s:sub(index, index) end
+	local function fromIndex() return s:sub(index) end
+	local function jsonErr(s)
+		local s = s:gsub("%%k", fromIndex():sub(1, math.min(#fromIndex(), 24)))
+		error("JSONError: "..s)
+	end
+	local function isNontrivialToken(x)
+		if x == JSON_ENUM.comma then return false end
+		if x == JSON_ENUM.colon then return false end
+		for k, v in pairs(JSON_ENUM) do if x == v then return true end end return false
+	end
+	-- Generate intermediate form
+	repeat
+		local foundNumber 		= not not currentChar():match("[-%d]")
+		local foundString 		= currentChar() == '"'
+		local foundObject 		= currentChar() == '{'
+		local foundObjectEnd 	= currentChar() == '}'
+		local foundArray 		= currentChar() == '['
+		local foundArrayEnd 	= currentChar() == ']'
+		local foundComma 		= currentChar() == ','
+		local foundColon 		= currentChar() == ':'
+		if currentChar():match("%s") then
+			if currentChar() == '\n' then
+				line = line + 1
+				lastRecordedChar = index
 			end
-			if currentChar == '"' then break end
-			foundString = foundString .. currentChar
-			goto continue
-
-			::controlChar::
-			if currentChar == '"' then
-				foundString = foundString .. '"'
-			elseif currentChar == '\\' then
-				foundString = foundString .. '\\'
-			elseif currentChar == '/' then
-				foundString = foundString .. '/'
-			elseif currentChar == 'b' then
-				foundString = foundString:sub(1, -2)
-			elseif currentChar == 'f' then
-				foundString = foundString .. '\f'
-			elseif currentChar == 'n' then
-				foundString = foundString .. '\n'
-			elseif currentChar == 'r' then
-				foundString = foundString .. '\r'
-			elseif currentChar == 't' then
-				foundString = foundString .. '\t'
-			elseif currentChar == 'u' then
-				goto unicode
-			else
-				warn((
-					"JSON Warning: No escape sequence '\\%s' is recognized. Inserting just %s instead."
-				):format(currentChar, currentChar))
-			end
-			goto continue
-
-			::unicode::
-			do
-				local code = s:sub(index + 1, index + 4)
-				if code:match("%X") then
-					error((
-						"JSON Error: Nonhexadecimal digit while decoding \\u.\nContext: '%s'"
-					):format(s:sub(math.max(1, index - 16), math.min(#s, index + 16))))
-				end
-				--shut the fuck up please thank you
-				---@diagnostic disable-next-line: undefined-field
-				foundString = foundString .. utf8.char(tonumber(code, 16))
-				index = index + 4
-			end
-			goto continue
-
-			::continue::
 			index = index + 1
+			goto continue
 		end
-		return foundString, index, "string"
-	end
 
-	-- Arrays
-	if s:match("^%s*%[") then
-		local list = {}
-		local index = s:find('%[') + 1
-		while true do
-			if s:match("%s*%]", index) then
-				index = index + #s:match("%s*%]", index)
-				break
-			end
-			if s:match("%s*,", index) then
-				index = index + #s:match("%s*,", index)
-				goto continue
-			end
-			local parsed, length = JSON.parse(s:sub(index))
-			table.insert(list, parsed)
-			index = index + length
+		for constType, constValue in pairs(CONSTANTS) do if s:match("^"..constType) then
+			pushIndex()
+			push(constValue)
+			index = index + #constType
+			goto continue
+		end end
 
-			::continue::
+		if foundNumber then
+			pushIndex()
+			local sign = fromIndex():match("^%-?")
+			index = index + #sign
+			local int = fromIndex():match("^[1-9]%d*") or fromIndex():match("^(0)%D")
+			xpcall(
+				assert,
+				jsonErr,
+				int,
+				("Line %i Char %i: Malformed Number. Context: %%k"):format(line,index-lastRecordedChar)
+			)
+			index = index + #int
+			local fraction = fromIndex():match("^%.%d+") or ''
+			index = index + #fraction
+			local exponent = fromIndex():match("^[Ee][+-]?%d+") or ''
+			index = index + #exponent
+			local number =
+				(sign == '-' and -1 or 1)
+				* (tonumber(int .. fraction))
+				* 10^(tonumber(exponent:sub(2)) or 0)
+			push(number)
+			goto continue
 		end
-		return list, index, "array"
-	end
 
-	-- Objects
-	if s:match("^%s*{") then
-		local dict = {}
-		local index = s:find('{') + 1
-		local expectingString = true
-		local strindex = ""
-		local isFirstElement = true
-		while true do
-			if expectingString then
-				if s:sub(index):match("^%s*}") then
-					index = index + #s:sub(index):match("^%s*}")
-					break
-				end
-				if not isFirstElement then
-					if s:sub(index):match("^%s*,") then
-						index = index + 1
-					else
-						error((
-							"JSON Error: Missing separator comma.\nContext '%s'."
-						):format(s:sub(math.max(1, index - 16), math.min(#s, index + 16))))
+		if foundString then
+			pushIndex()
+			local stringToAdd = ""
+			repeat
+				index = index + 1
+				if currentChar() == '\\' then
+					index = index + 1
+					if currentChar() == '\n' then
+						jsonErr("Malformed string. Context: %k")
 					end
+					if currentChar() == '"' then stringToAdd = stringToAdd .. '"' end
+					if currentChar() == '/' then stringToAdd = stringToAdd .. '/' end
+					if currentChar() == 'b' then stringToAdd = stringToAdd .. 'b' end
+					if currentChar() == 'f' then stringToAdd = stringToAdd .. 'f' end
+					if currentChar() == 'n' then stringToAdd = stringToAdd .. 'n' end
+					if currentChar() == 'r' then stringToAdd = stringToAdd .. 'r' end
+					if currentChar() == 't' then stringToAdd = stringToAdd .. 't' end
+					if currentChar() == '\\' then stringToAdd = stringToAdd .. '\\' end
+					if currentChar() == 'u' then
+						index = index + 1
+						local fourHex = fromIndex():match("^%x%x%x%x")
+						xpcall(assert, jsonErr, fourHex, "Expected four hex characters, found %k...")
+						---@diagnostic disable-next-line
+						stringToAdd = stringToAdd .. utf8.char(tonumber(fourHex, 16))
+						index = index + 4
+						index = index - 1
+					end
+					index = index + 1
+				elseif currentChar() == '"' then break
+				else
+					stringToAdd = stringToAdd .. currentChar()
 				end
-				local str, len, ty = JSON.parse(s:sub(index))
-				if ty ~= "string" then
-					error((
-						"JSON Error: Didn't get string for the first half of an object.\nContext '%s'."
-					):format(s:sub(math.max(1, index - 16), math.min(#s, index + 16))))
-				end
-				strindex = str
-				index = index + len
-				if not s:sub(index):match("^%s*:") then
-					error((
-						"JSON Error: No colon found for object.\nContext '%s'"
-					):format(s:sub(math.max(1, index - 16), math.min(#s, index + 16))))
-				end
-				index = index + #s:sub(index):match("^%s*:")
-				expectingString = false
-			else
-				local val, len = JSON.parse(s:sub(index))
-				index = index + len
-				dict[strindex] = val
-				isFirstElement = false
-				expectingString = true
+			until false
+			push(stringToAdd)
+			index = index + 1
+			goto continue
+		end
+
+		if foundObject 		then pushIndex() push(JSON_ENUM.objInit	) index = index + 1 goto continue end
+		if foundObjectEnd 	then pushIndex() push(JSON_ENUM.objExit	) index = index + 1 goto continue end
+		if foundArray 		then pushIndex() push(JSON_ENUM.arrInit	) index = index + 1 goto continue end
+		if foundArrayEnd 	then pushIndex() push(JSON_ENUM.arrExit	) index = index + 1 goto continue end
+		if foundComma 		then pushIndex() push(JSON_ENUM.comma	) index = index + 1 goto continue end
+		if foundColon 		then pushIndex() push(JSON_ENUM.colon	) index = index + 1 goto continue end
+
+		jsonErr("Unknown symbol. Context: %k")
+
+		::continue::
+	until fromIndex() == ""
+
+	local stackTop = 1
+	local objectStack = {}
+	local tokenIndex = 1
+	---@type stack<"array" | "object">
+	local modeStack = {}
+	local arrayTop = 1
+	local function pushToArray()
+		local array = objectStack[stackTop-2]
+		local toBePushed = objectStack[stackTop - 1]
+		array[arrayTop] = toBePushed
+		arrayTop = arrayTop + 1
+		stackTop = stackTop - 1
+	end
+	local function pushToObject()
+		local Object = objectStack[stackTop-4]
+		local key = objectStack[stackTop-3]
+		local value = objectStack[stackTop-1]
+		Object[key] = value
+		stackTop = stackTop - 3
+	end
+	while true do
+		local current = intermediate[tokenIndex]
+		local currentPos = intermediatePositions[tokenIndex]
+		if current == nil then break end
+		if not isNontrivialToken(current) then
+			if not next(modeStack) and next(objectStack) then
+				jsonErr(("Line %i Char %i: Expected <eof>, got %%k"):format(currentPos[1], currentPos[2]))
+			end
+			objectStack[stackTop] = current
+			stackTop = stackTop + 1
+		else
+			if current == JSON_ENUM.arrInit then
+				objectStack[stackTop] = {}
+				stackTop = stackTop + 1
+				table.insert(modeStack, "array")
+				arrayTop = 1
+			elseif current == JSON_ENUM.arrExit then
+				pushToArray()
+				table.remove(modeStack)
+			elseif current == JSON_ENUM.objInit then
+				objectStack[stackTop] = {}
+				stackTop = stackTop + 1
+				table.insert(modeStack, "object")
+			elseif current == JSON_ENUM.objExit then
+				pushToObject()
+				table.remove(modeStack)
+			elseif current == JSON_ENUM.null then
+				stackTop = stackTop + 1
 			end
 		end
-		return dict, index, "object"
+		if modeStack[#modeStack] == "array" then
+			if objectStack[stackTop-1] == JSON_ENUM.comma then
+				stackTop = stackTop - 1
+				pushToArray()
+			end
+		elseif modeStack[#modeStack] == "object" then
+			if objectStack[stackTop-1] == JSON_ENUM.comma then
+				stackTop = stackTop - 1
+				pushToObject()
+			end
+		end
+		tokenIndex = tokenIndex + 1
+		
+		local cutStack = {}
+		for i = 1, stackTop-1 do cutStack[i] = objectStack[i] end
 	end
-
-	error((
-		"JSON Error: Unrecognized symbols.\nContext '%s'"
-	):format(s:sub(1, math.min(#s, 17))))
+	return objectStack[1]
 end
 
 ---Parses a json file from its path into an object.
