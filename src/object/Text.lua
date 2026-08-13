0173418bce3d6e6dedad8f5d57c072e0d3957afb
Ultrio <huyouyu42@gmail.com>
2026-08-13 20:51:20 +0800

    Fixed text alignment issues

--- src/object/Text.lua
+++ src/object/Text.lua
@@ -20,10 +20,16 @@ function Text:DrawSelf(pass)
 	if not self.Visible then return end
 	local x,y,w,h = self:GetAbsoluteDimensionTuple()
 	local px, py = self:GetPivotedPosition()
+	local adjustedX = px
+	local adjustedY = py
+	if self.HorizontalAlignment == "left" then adjustedX = px - w/2 end
+	if self.HorizontalAlignment == "right" then adjustedX = px + w/2 end
+	if self.VerticalAlignment == "top" then adjustedY = py - h/2 end
+	if self.VerticalAlignment == "bottom" then adjustedY = py + h/2 end
 	pass:setColor(self.Color, 1-self.Transparency)
 	pass:text(
 		self.Content,
-		px, py, 0, self.Scale, 0, 0, 0, 0,
+		adjustedX, adjustedY, 0, self.Scale, 0, 0, 0, 0,
 		self.MetersBeforeWrapping or 0,
 		self.HorizontalAlignment, self.VerticalAlignment
 )
